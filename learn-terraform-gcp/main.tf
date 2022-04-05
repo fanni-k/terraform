294506106c166c93278bb797c88323f79deac4ab
Fanni Kiss <strabancer@gmail.com>
2022-04-05 18:02:32 +0200

    add tf files

---
+++ learn-terraform-gcp/main.tf
@@ -0,0 +1,50 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "example_dataset"
+}
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
+
+resource "google_bigquery_table" "default" {
+  dataset_id = "example_dataset"
+  table_id   = "bar"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
+  }
+]
+EOF
+
+}
+
+resource "google_bigquery_table" "test" {
+  dataset_id = "example_dataset"
+  table_id   = "foo"
+
+}
---
+++ learn-terraform-gcp/terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "fanni-kiss-sandbox"
+credentials_file         = "C:/Users/Fanni/code/github/aliz-ai/terraform/learn-terraform-gcp/fanni-kiss-sandbox-aa207718416c.json"
